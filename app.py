2b3ba63e41087619ca245ec9f96381038f92e763
KetanAgrawal2002 <98745010+KetanAgrawal2002@users.noreply.github.com>
2023-07-09 11:42:59 +0530

    Update app.py
    added sample images option

--- app.py
+++ app.py
@@ -9,13 +9,34 @@ def query(file):
     return response.text
 
 def main():
-    st.title("Handwritten Form Text Extraction")
+    st.title("Handwritten Text Extraction")
     st.write("Upload an image of a handwritten form to extract the text.")
+
+     sample_images = [
+        "image.jpg",
+        "image2.jpg",
+        "image3.jpg"
+    ]
     
     uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
     
-    if uploaded_file is not None:
-        output = query(uploaded_file.read())
+   if uploaded_file is not None:
+        image_bytes = uploaded_file.read()
+        output = query(image_bytes)
+        st.write("Extracted Text:")
+        st.write(output)
+
+    selected_image = st.sidebar.selectbox("Select Sample Image", sample_images)
+
+    st.subheader("Selected Image:")
+
+    image = Image.open(selected_image)
+    st.image(image, use_column_width=True)
+
+    if st.button("Use this Image"):
+        with open(selected_image, "rb") as f:
+            image_bytes = f.read()
+        output = query(image_bytes)
         st.write("Extracted Text:")
         st.write(output)
 
